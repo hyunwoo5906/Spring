7b93f4fb66c116c9b31bf5bca81a53f86f7782e4
hyunwoo5906 <33214649+hyunwoo5906@users.noreply.github.com>
2018-06-15 18:12:03 +0900

    Add files via upload
    spring security 공부

---
+++ securitymybatis/src/main/java/net/skhu/SecuritymybatisApplication.java
@@ -0,0 +1,12 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritymybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritymybatisApplication.class, args);
+	}
+}
